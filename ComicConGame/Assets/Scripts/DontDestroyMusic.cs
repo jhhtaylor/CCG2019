d3a681cd46903ca84bb11856aeeb03d37c8cc452
jhhtaylor <jhhtaylor@gmail.com>
2019-09-22 20:00:17 +0200

    Main Menu Done

---
+++ ComicConGame/Assets/Scripts/DontDestroyMusic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyMusic : MonoBehaviour
+{
+
+    void Awake()
+    {
+        //dont destroy the music across scenes
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
